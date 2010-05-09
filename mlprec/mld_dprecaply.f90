4c7bbe2d56a0e88da0cdb42a2794cf76538a58f1
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2010-05-09 07:27:40 +0000

    mld2p4-2 Fixes for USE statements on XLF.

--- mlprec/mld_dprecaply.f90
+++ mlprec/mld_dprecaply.f90
@@ -75,7 +75,7 @@ subroutine mld_dprecaply(prec,x,y,desc_data,info,trans,work)
 
   use psb_sparse_mod
   use mld_inner_mod, mld_protect_name => mld_dprecaply
-  use mld_prec_mod, mld_protect_name => mld_dprecaply
+ !  use mld_prec_mod, mld_protect_name => mld_dprecaply
   
   implicit none
   
@@ -209,7 +209,7 @@ subroutine mld_dprecaply1(prec,x,desc_data,info,trans)
 
   use psb_sparse_mod
   use mld_inner_mod, mld_protect_name => mld_dprecaply1
-  use mld_prec_mod, mld_protect_name => mld_dprecaply1
+!  use mld_prec_mod, mld_protect_name => mld_dprecaply1
 
   implicit none
 
