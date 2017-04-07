89e660fc6734dfe31e938982398368a5d67b4470
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2017-04-07 15:48:36 +0000

    mld2p4-2:
    1. Fix license statement,
    2. Fix configure, take out DHAVE_MOVE_ALLOC and DHAVE_ISO_C_BIND since
       they are now required;
    3. Start working on prec%build/init/free interfaces.

--- mlprec/mld_d_prec_mod.f90
+++ mlprec/mld_d_prec_mod.f90
@@ -1,42 +1,42 @@
-!!$ 
-!!$ 
-!!$                           MLD2P4  version 2.1
-!!$  MultiLevel Domain Decomposition Parallel Preconditioners Package
-!!$             based on PSBLAS (Parallel Sparse BLAS version 3.4)
-!!$  
-!!$  (C) Copyright 2008, 2010, 2012, 2015, 2017 
-!!$
-!!$      Salvatore Filippone    Cranfield University
-!!$      Ambra Abdullahi Hassan University of Rome Tor Vergata
-!!$      Alfredo Buttari        CNRS-IRIT, Toulouse
-!!$      Pasqua D'Ambra         ICAR-CNR, Naples
-!!$      Daniela di Serafino    University of Campania "L. Vanvitelli", Caserta
-!!$ 
-!!$  Redistribution and use in source and binary forms, with or without
-!!$  modification, are permitted provided that the following conditions
-!!$  are met:
-!!$    1. Redistributions of source code must retain the above copyright
-!!$       notice, this list of conditions and the following disclaimer.
-!!$    2. Redistributions in binary form must reproduce the above copyright
-!!$       notice, this list of conditions, and the following disclaimer in the
-!!$       documentation and/or other materials provided with the distribution.
-!!$    3. The name of the MLD2P4 group or the names of its contributors may
-!!$       not be used to endorse or promote products derived from this
-!!$       software without specific written permission.
-!!$ 
-!!$  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-!!$  ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-!!$  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-!!$  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE MLD2P4 GROUP OR ITS CONTRIBUTORS
-!!$  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-!!$  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-!!$  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-!!$  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-!!$  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-!!$  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-!!$  POSSIBILITY OF SUCH DAMAGE.
-!!$ 
-!!$
+!   
+!   
+!                             MLD2P4  version 2.1
+!    MultiLevel Domain Decomposition Parallel Preconditioners Package
+!               based on PSBLAS (Parallel Sparse BLAS version 3.4)
+!    
+!    (C) Copyright 2008, 2010, 2012, 2015, 2017 
+!  
+!        Salvatore Filippone    Cranfield University
+!        Ambra Abdullahi Hassan University of Rome Tor Vergata
+!        Alfredo Buttari        CNRS-IRIT, Toulouse
+!        Pasqua D'Ambra         ICAR-CNR, Naples
+!        Daniela di Serafino    University of Campania "L. Vanvitelli", Caserta
+!   
+!    Redistribution and use in source and binary forms, with or without
+!    modification, are permitted provided that the following conditions
+!    are met:
+!      1. Redistributions of source code must retain the above copyright
+!         notice, this list of conditions and the following disclaimer.
+!      2. Redistributions in binary form must reproduce the above copyright
+!         notice, this list of conditions, and the following disclaimer in the
+!         documentation and/or other materials provided with the distribution.
+!      3. The name of the MLD2P4 group or the names of its contributors may
+!         not be used to endorse or promote products derived from this
+!         software without specific written permission.
+!   
+!    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+!    ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+!    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+!    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE MLD2P4 GROUP OR ITS CONTRIBUTORS
+!    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+!    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+!    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+!    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+!    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+!    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+!    POSSIBILITY OF SUCH DAMAGE.
+!   
+!  
 ! File: mld_d_prec_mod.f90
 !
 ! Module: mld_d_prec_mod
@@ -71,23 +71,23 @@ module mld_d_prec_mod
          & mld_d_cprecseti, mld_d_cprecsetc, mld_d_cprecsetr
   end interface mld_precset
 
-  interface mld_precbld
-    subroutine mld_dprecbld(a,desc_a,prec,info,amold,vmold,imold)
-      import :: psb_dspmat_type, psb_desc_type, psb_dpk_, &
-           & psb_d_base_sparse_mat, psb_d_base_vect_type, &
-           & psb_i_base_vect_type, mld_dprec_type, psb_ipk_
-      implicit none
-      type(psb_dspmat_type), intent(in), target          :: a
-      type(psb_desc_type), intent(inout), target           :: desc_a
-      type(mld_dprec_type), intent(inout), target        :: prec
-      integer(psb_ipk_), intent(out)                       :: info
-      class(psb_d_base_sparse_mat), intent(in), optional :: amold
-      class(psb_d_base_vect_type), intent(in), optional  :: vmold
-      class(psb_i_base_vect_type), intent(in), optional  :: imold
-      !      character, intent(in),optional             :: upd
-    end subroutine mld_dprecbld
-  end interface mld_precbld
-
+!!$  interface mld_precbld
+!!$    subroutine mld_dprecbld(a,desc_a,prec,info,upd,amold,vmold,imold)
+!!$      import :: psb_dspmat_type, psb_desc_type, psb_dpk_, &
+!!$           & psb_d_base_sparse_mat, psb_d_base_vect_type, &
+!!$           & psb_i_base_vect_type, mld_dprec_type, psb_ipk_
+!!$      implicit none
+!!$      type(psb_dspmat_type), intent(in), target          :: a
+!!$      type(psb_desc_type), intent(inout), target           :: desc_a
+!!$      type(mld_dprec_type), intent(inout), target        :: prec
+!!$      integer(psb_ipk_), intent(out)                       :: info
+!!$      class(psb_d_base_sparse_mat), intent(in), optional :: amold
+!!$      class(psb_d_base_vect_type), intent(in), optional  :: vmold
+!!$      class(psb_i_base_vect_type), intent(in), optional  :: imold
+!!$      character, intent(in),optional             :: upd
+!!$    end subroutine mld_dprecbld
+!!$  end interface mld_precbld
+!!$
   interface mld_hierarchy_bld
     subroutine mld_d_hierarchy_bld(a,desc_a,prec,info)
       import :: psb_dspmat_type, psb_desc_type, psb_dpk_, &
